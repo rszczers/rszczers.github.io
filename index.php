92b53584a0d400dc3951337981326538707a8dde
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-11-28 14:58:12 +0100

    pgp

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+Rafał Szczerski<br>
+<a href="http://github.com/rszczers">https://github.com/rszczers</a><br>
+rszczers@freenode<br>
+rafal(dot)szczerski(at)gmail(dot)com
+<p>
+    <pre>
+	<code>
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v1
+
+mQENBFZQji0BCADIrxzNVfyNd9pjl7T/XqI9X8rrjCna31gnPNcoUVBOi47oGMpr
+RhQe60yWfJNN+R0ezHufhJmP/Od1uVU+H74vrGh6Sr0cpbsHPIm3s4btmeBFzijF
+k3BQ16ObVHU2yWcqStMT7yZtmT+AMaPa2PY53S1CpTwPNd3CwX8I2mPy6DPTM5lK
+I4ngBb5S/9Sdngf27NRFR0Kn72AZsVpLhFmUpRSv3uL+JpsEFvrmtmjU51UuZKSK
+EnlYKAgrVJ0GIKQupMGVgxU2XnieC1xbAi2ulZlpPm40Bv0Xw+T6/0KXUAr5icg4
+A8Y/JIC4GUbirD0uSdc6DKDW4Jl8DP53oOnxABEBAAG0N1JhZmHFgiBTemN6ZXJz
+a2kgKHJzemN6ZXJzKSA8cmFmYWwuc3pjemVyc2tpQGdtYWlsLmNvbT6JATgEEwEC
+ACIFAlZQji0CGwMGCwkIBwMCBhUIAgkKCwQWAgMBAh4BAheAAAoJEKMa7zVR4rwh
+I8EH/R9Uvp6ulxzb/3IhQxJl8l9B5ZfWgKeWtsOGalFvOd69JME/JXiJ31xAS+rd
+CPaH1ZO0N+mmTIzTSi1RqM9CDpHDPiCd+ssB3cv8uFig1CYjmdNLlPLxtP79ad0j
+kyjW/yPrZquLr5wwQ5q9saEh/lkBhyXIP7I651Ra/rf32gzjT3wqBwh5lr8D5Ttp
+0m8DDB6xe1eb3pSVKseX6QRxKv6/n9tDMim6/GPNDydPTdLQwBFbhvM1npoxvteD
+Fpfe3GynpxIQQrgXAKKSb6zaLSg1NEoJyb68upOKmU4uWmEKWs8naTQfoatIoYnY
+5mfIz6wqkvatt/D6ND4DSttmDvS5AQ0EVlCOLQEIAKigurSLNowQhMrg90VUJK9t
+rCtdL7PEodmUVNufWBj3KagQlz1ejgmTzwSsaLiarrMie8x7pNPOOXMzo0/FibWa
+uVTU0CS0yJd8p+CiS0i35lMXRt5QTnaOgXDISW5EdEkjHNvDF43ktinI1B/yE2D6
+6rcSm8pqAoN227TRG7CH3IQgiXTLfvwYFjnyjMdsLAoBgJPbGl1dWcGU6ULWXkyM
+meaAVi2xzHiTJN3aptnXpFTXcEuHB0ZwzEuOhjUpbfAA1bycmjb2G18pSUyGkyTY
+50m3OW8a20il1Rx3z9vlPuE+Jf9lZcW32cFUfhDbbZXlrokRFDYHIzCFmX5P2pkA
+EQEAAYkBHwQYAQIACQUCVlCOLQIbDAAKCRCjGu81UeK8IbwLB/0Xsdt/oWdUXxjJ
+bDZQfCH+BA9QPT6QB9Qmkza2yQi5Vksh0sT6YSbYXBKNjpRzzs1Yc0mwjVKZRa2z
+kJcpsUXjjImRiHE1kZItL3VmrqWOSXuPEWc65/NQn6AcO2l2kCo5dMwBX0EAt4uJ
+QKkf+8LhLRJR3WpiSPeDLgpXNnvLdf/y6wuEMEf9tZAYk7ZYiWF59ov5NrXxH1Uy
+VI4XYmVGkH6uvAVoDZO9nlmjT+LKm3I1oaSRRapQG8sPNEFjCVp+lg7JKc1uccso
+N07YxpvOz/kvzI5zSH9BCM83tPpcvp1OdzteTrltVM5/3Larvw9YdhalMA5+BFLo
+AQoBVt3q
+=33kI
+-----END PGP PUBLIC KEY BLOCK-----
+        </code>
+    </pre>
+</p>
+</body>
+</html>
